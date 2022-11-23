fee0365646d1c18a040dba55163bec03de0a0f05
milkcoke <mbh023@gmail.com>
2022-11-23 21:14:37 +0900

    feat: Add controllers about members
    Add join, listing members controllers.

---
+++ src/main/java/falcon/springpractice/SpringConfig.java
@@ -0,0 +1,19 @@
+package falcon.springpractice;
+
+import falcon.springpractice.repository.MemberRepository;
+import falcon.springpractice.repository.MemoryMemberRepository;
+import falcon.springpractice.service.MemberService;
+import org.springframework.context.annotation.Bean;
+
+public class SpringConfig {
+
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+}
