7770e17d157357f76d1b0d9b2ea07b289028dde0
milkcoke <mbh023@gmail.com>
2022-11-26 23:18:28 +0900

    build: Configure beans of ORM
    Enroll JPA and Jdbc datasource Spring beans configuration.

--- src/main/java/falcon/springpractice/SpringConfig.java
+++ src/main/java/falcon/springpractice/SpringConfig.java
@@ -1,19 +1,42 @@
 package falcon.springpractice;
 
+import falcon.springpractice.repository.JdbcTemplateMemberRepository;
+import falcon.springpractice.repository.JpaMemberRepository;
 import falcon.springpractice.repository.MemberRepository;
-import falcon.springpractice.repository.MemoryMemberRepository;
 import falcon.springpractice.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+
+// Open-Close Principal (OCP) : 확장에는 열려있고, 변경에는 닫혀있다.
+@Configuration
 public class SpringConfig {
 
-//    @Bean
-//    public MemberService memberService() {
-//        return new MemberService(memberRepository());
-//    }
-//
-//    @Bean
-//    public MemberRepository memberRepository() {
+//    private final DataSource dataSource;
+    @PersistenceContext
+    private final EntityManager em;
+
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    // Dependencies Injection 으로
+    // 기존의 코드를 전혀 손대지 않고, 설정만으로 구현 클래스를 변경할 수 있다.
+    @Bean
+    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-//    }
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
+    }
 }
--- src/main/java/falcon/springpractice/service/MemberService.java
+++ src/main/java/falcon/springpractice/service/MemberService.java
@@ -4,11 +4,13 @@ import falcon.springpractice.domain.Member;
 import falcon.springpractice.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
+//@Service
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
