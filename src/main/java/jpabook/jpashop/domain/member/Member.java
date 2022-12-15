78476389902da979d0c014cbeefe6ae844fc21f0
milkcoke <mbh023@konkuk.ac.kr>
2022-12-15 22:41:24 +0900

    feat: Define `Member` entity

--- src/main/java/jpabook/jpashop/domain/member/Member.java
+++ src/main/java/jpabook/jpashop/domain/member/Member.java
@@ -1,17 +1,28 @@
 package jpabook.jpashop.domain.member;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.address.Address;
+import jpabook.jpashop.domain.order.Order;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
 public class Member {
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String userName;
 
+    @Embedded
+    private Address address;
+
+    // 연관 관계의 주인이 아닌 경우 읽기 전용이 됨.
+    // `member` field in order 에 의해 매핑되었다는 것을 명시.
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList();
 }
 
