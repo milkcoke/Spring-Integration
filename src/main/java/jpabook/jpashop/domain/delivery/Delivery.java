a3d530c7c5684467bdfe38af006eb20cc5b1e079
milkcoke <mbh023@konkuk.ac.kr>
2022-12-15 22:43:01 +0900

    feat: Define `Delivery` entity

---
+++ src/main/java/jpabook/jpashop/domain/delivery/Delivery.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain.delivery;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.address.Address;
+import jpabook.jpashop.domain.order.Order;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    private Address address;
+
+    // Ordinal 은 순서가 바뀌면 장애가 날 수 있으므로
+    // 되도록 String 을 사용하라.
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus deliveryStatus; // Ready || Complete
+}
