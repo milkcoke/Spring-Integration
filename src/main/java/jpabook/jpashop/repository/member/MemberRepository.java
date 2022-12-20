263cb487b73d39a96a025f9b14d464f99ab49a16
milkcoke <mbh023@konkuk.ac.kr>
2022-12-20 20:59:08 +0900

    feat: Update `MemberRepository`
    Add `findAll()` and `findByName()` methods.

--- src/main/java/jpabook/jpashop/repository/member/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/member/MemberRepository.java
@@ -5,6 +5,8 @@ import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.member.Member;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class MemberRepository {
 
@@ -15,11 +17,27 @@ public class MemberRepository {
     private EntityManager em;
 
     public Long save(Member member) {
+        // DB Insert query 가 날라감.
         em.persist(member);
         return member.getId();
     }
 
     public Member findById(Long id) {
+        // 1st: Return Type, 2nd: Primary key
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        // jpql
+        // sql 은 테이블 대상으로 쿼리하는 반면, jpql 은 객체를 Entity 객체를 대상으로 쿼리함.
+        // 1st: query, 2nd: return type
+        return em.createQuery("select m from Member m", Member.class)
+                        .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.userName = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
     }
 }
