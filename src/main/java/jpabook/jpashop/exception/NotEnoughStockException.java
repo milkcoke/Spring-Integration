3c6a50b7e1b73b295d10c416536adf8db9de2f84
milkcoke <mbh023@konkuk.ac.kr>
2022-12-21 21:21:46 +0900

    feat: Add Item stock management methods
    Add `addStock()` and `removeStock()` method.

---
+++ src/main/java/jpabook/jpashop/exception/NotEnoughStockException.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.exception;
+
+public class NotEnoughStockException extends RuntimeException {
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    // 예외 StackTrace 추적을 위한 메소드
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NotEnoughStockException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
