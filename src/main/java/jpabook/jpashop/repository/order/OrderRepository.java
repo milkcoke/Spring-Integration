a8dcd1958272937e12def82f3d8e12fd7d745cdf
milkcoke <mbh023@konkuk.ac.kr>
2022-12-26 18:50:03 +0900

    feat: Add OrderSearch for dynamic query and Update findAll() method in OrderRepository
    Use static query, not supported yet dynamic query.

--- src/main/java/jpabook/jpashop/repository/order/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/OrderRepository.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.order.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
@@ -18,9 +20,18 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//
-//    }
+    // static query
+    public List<Order> findAll(OrderSearch orderSearch) {
+        // how to resolve dynamic query
+        var orders = em.createQuery("SELECT o FROM Order o JOIN o.member m" +
+                " WHERE o.status = :status" +
+                " AND m.name LIKE :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                // LIMIT 1000
+                .setMaxResults(1000)
+                .getResultList();
+    }
 
 
 }
