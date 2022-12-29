59142dce7c78af7f0e5824ac87f7b185ccf05d7d
milkcoke <mbh023@konkuk.ac.kr>
2022-12-29 21:04:04 +0900

    feat: Add find all member controller

--- src/main/java/jpabook/jpashop/controller/member/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/member/MemberController.java
@@ -11,11 +11,21 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
+    }
 
     @GetMapping(value = "/members/new")
     public String createForm(Model model) {
@@ -41,4 +51,5 @@ public class MemberController {
         memberService.join(member);
         return "redirect:/";
     }
+
 }
