e42c43034f885b0099195c0a5b2c4296552ab998
milkcoke <mbh023@gmail.com>
2023-01-02 17:54:30 +0900

    feat: Add updating book item information
    Update ItemController and Add updateItemForm.html web page.

--- src/main/java/jpabook/jpashop/controller/item/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/item/ItemController.java
@@ -2,12 +2,12 @@ package jpabook.jpashop.controller.item;
 
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.item.ItemRepository;
 import jpabook.jpashop.service.item.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +29,41 @@ public class ItemController {
         model.addAttribute("bookForm", new BookForm());
         return "items/createItemForm";
     }
+
+    @GetMapping("items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book item = (Book) itemService.findOneById(itemId);
+        // 이렇게 하는게 짜증나긴 하지만 걍 해야함.
+        BookForm bookForm = new BookForm();
+        bookForm.setId(item.getId());
+        bookForm.setName(item.getName());
+        bookForm.setPrice(item.getPrice());
+        bookForm.setStockQuantity(item.getStockQuantity());
+        bookForm.setAuthorName(item.getAuthorName());
+        bookForm.setIsbn(item.getIsbn());
+
+        model.addAttribute("bookForm", bookForm);
+
+        return "items/updateItemForm";
+    }
+
+    @PutMapping("items/{itemId}")
+    // thymeleaf html 파일에서 object 이름이 그대로 넘어옴
+    public String updateItem(@PathVariable String itemId, @ModelAttribute("bookForm") BookForm bookForm) {
+        Book book = (Book) itemService.findOneById(Long.parseLong(itemId));
+
+        // bookForm 은 대체 ID를 어떻게 가지고 있는거?
+        book.setId(bookForm.getId());
+        book.setName(bookForm.getName());
+        book.setPrice( bookForm.getPrice());
+        book.setStockQuantity(bookForm.getStockQuantity());
+        book.setAuthorName(bookForm.getAuthorName());
+        book.setIsbn(bookForm.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
+
 
     @PostMapping("/items/new")
     public String create(BookForm form) {
