462565c00fbcf9d93eb2ef6047638b699c3f3f9c
milkcoke <mbh023@gmail.com>
2023-01-04 09:36:04 +0900

    refactor: Add `@NoArgsConstructor` annotation instead of explicit constructor

--- src/main/java/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -2,19 +2,18 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @DiscriminatorValue("b")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter @Setter
 public class Book extends Item {
     private String authorName;
     private String isbn;
-
-    protected Book() {
-
-    }
 
     public static Book createBook(String name, int price, int stockQuantity, String authorName, String isbn) {
         Book book = new Book();
