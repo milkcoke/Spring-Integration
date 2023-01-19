66ce7a3b10f14ad5ae033567d2c02c357d121a8b
milkcoke <mbh023@gmail.com>
2023-01-19 22:35:33 +0900

    feat: Override equals and hashCode method for comparing equivalence
    Update Address embeddable type.

--- src/main/java/com/example/jpapractice/domain/member/Address.java
+++ src/main/java/com/example/jpapractice/domain/member/Address.java
@@ -6,6 +6,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Getter
 @Embeddable // 임베디드 타입 선언 필수
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -15,6 +17,19 @@ public class Address {
     private String city;
     private String street;
     private String zipCode;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(country, address.country) && Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zipCode, address.zipCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(country, city, street, zipCode);
+    }
 
     public Address(String country, String city, String street, String zipCode) {
         this.country = country;
