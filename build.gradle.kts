842853e8e35ca30d2ce605bd175db767a37f17b5
milkcoke <mbh023@gmail.com>
2023-11-05 03:14:26 +0900

    build: Add jib using container `jvmFlags` option with `spring.actives.profile`

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.3"
+    id("com.google.cloud.tools.jib") version "3.4.0"
     kotlin("jvm") version "1.9.20"
     kotlin("plugin.spring") version "1.9.20"
 }
@@ -26,12 +27,25 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
+jib {
+    from {
+        image = "openjdk:20-jdk"
+    }
+    to {
+        image = "milkcoke/spring-yaml"
+        tags = setOf(project.version.toString())
+    }
+    container {
+        jvmFlags = listOf("-Dspring.profiles.active=dev")
+    }
+}
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
         jvmTarget = "20"
     }
 }
+
 
 tasks.withType<Test> {
     useJUnitPlatform()
