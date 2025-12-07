8fd3a046b86464e2f3ee2aeb91d4783e68259799
milkcoke <mbh023@gmail.com>
2025-12-08 01:11:10 +0900

    fix: Rename the basePackages in the AutoAppConfig

--- spring-core/src/main/java/spring/core/config/AutoAppConfig.java
+++ spring-core/src/main/java/spring/core/config/AutoAppConfig.java
@@ -2,14 +2,13 @@ package spring.core.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
         // 패키지를 찾을 시작 위치를 지정할 수 있음.
-        basePackages = "milkcoke.core",
+        basePackages = "spring.core"
         // Configuration 붙은 클래스는 제외 (ex. milkcoke.core.config.AppConfig)하여 충돌 방지
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+//        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
     // DI 를 생성자 없이 어떻게 할 것인가? => AutoWired!
--- spring-core/src/main/java/spring/core/config/WebConfig.java
+++ spring-core/src/main/java/spring/core/config/WebConfig.java
@@ -1,11 +1,10 @@
 package spring.core.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig  implements WebMvcConfigurer {
+//@Configuration
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureApiVersioning(ApiVersionConfigurer configurer) {
